15256db1a9c10fc3e4521500c20a99a341361027
RisingShinignMoon <vladkholodenko@gmail.com>
2023-05-30 09:23:13 +0300

    CD-17 created page with cars

--- client/app/shop/page.tsx
+++ client/app/shop/page.tsx
@@ -8,7 +8,31 @@ export default async function Shop(){
     // const cars = {cars:[]}
     return(
         <>
-        
+        <div className="search-field">
+            <input type="text" placeholder="Search..."/>
+            <div className="search-buttons">
+            <button className="filter-button">Фильтри</button>
+            <button className="search-button">Шукати</button>
+        </div>
+        </div>
+            <div className="container">
+                <div className="grid">
+                        {cars["cars"].map((car: any)=>(
+                            <div className="item">
+                                <div className="item-image">
+                                <img src="https://gumlet.assettype.com/afkgaming/2022-02/e063c290-ee60-4c95-b732-82972cbf3298/Untitled_design__63___1_.jpg?w=411&format=webp&compress=true" alt="Car Image"/>
+                                </div>
+                                <div className="item-info">
+                                <h3>{car.brand} {car.model}</h3>
+                                <p>{car.price}</p>
+                                <p>{car.horsepower}</p>
+                                <p>{car.max_speed}</p>
+                                <p>0-100 km/h: {car.acceleration_to_100} seconds</p>
+                                </div>
+                            </div>
+                        ))}
+                </div>
+            </div>
         </>
     )
 }
