bb5b58349337cabb93dc049a0e1952efc586176f
RisingShinignMoon <vladkholodenko@gmail.com>
2023-06-01 15:15:37 +0300

    little change in request to server

--- client/services/getCars.ts
+++ client/services/getCars.ts
@@ -1,5 +1,5 @@
 export const getCars = async (page:number) => {
-    const cars = await fetch(`http://localhost:8080/cars?page=${page}&limit=6`)
+    const cars = await fetch(`http://localhost:8080/cars?page=${page}`)
     // const result = await cars.json();
     // return result["cars"];
     return cars.json();
