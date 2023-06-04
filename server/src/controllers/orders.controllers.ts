a4777d8a4ba6fd978e93a928f9dac16af077b4e1
Alexey Latyshev <latyshevskiymail@gmail.com>
2023-06-04 10:16:46 +0300

    CD-48 feat: controller to receive all orders

--- server/src/controllers/orders.controllers.ts
+++ server/src/controllers/orders.controllers.ts
@@ -1,1 +1,22 @@
-export const getAllOrders = async (request, response) => {};
+import { Request, Response } from 'express';
+
+import Order from '@/models/Order';
+
+export const getAllOrders = async (request: Request, response: Response) => {
+  const { page = 1, limit = 9, sort = 'createdAt', order = 'asc' } = request.query;
+
+  try {
+    const orderValue = order === 'asc' ? 1 : order === 'desc' ? -1 : -1;
+    const list = await Order.find({ userId: request.user?._id })
+      .sort({ [sort as string]: orderValue })
+      .limit(+limit * 1)
+      .skip((+page - 1) * +limit)
+      .exec();
+
+    response.status(200).send(list);
+  } catch (error) {
+    return response.status(401).json({
+      message: 'unauthorized',
+    });
+  }
+};
--- server/src/routes/orders.routes.ts
+++ server/src/routes/orders.routes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 
+import { getAllOrders } from '@/controllers/orders.controllers';
 import verification from '@/middleware/verification.middleware';
 
 const router = express.Router({ mergeParams: true });
 
-router.get('/', verification);
+router.get('/', verification, getAllOrders);
 
 export default router;
