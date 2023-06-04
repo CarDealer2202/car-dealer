31cfa30c66d4b814534cd9c7076cfe51da656d55
Alexey Latyshev <latyshevskiymail@gmail.com>
2023-06-04 09:17:58 +0300

    CD-45 feat: controller for authorization

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -14,30 +14,30 @@ export const registerUser = async (
   try {
     const errors = validationResult(request);
 
-    if (errors.isEmpty()) {
-      const existedUser = await User.findOne({ email });
-
-      if (existedUser) {
-        return response.status(409).send({
-          message: 'user with this email is already registered',
-        });
-      } else {
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = await User.create({
-          ...request.body,
-          password: hashedPassword,
-        });
-
-        const tokens = tokenService.generateTokens({ _id: newUser._id });
-        await tokenService.save(newUser._id, tokens.refreshToken);
-
-        return response.status(201).send({ ...tokens, userId: newUser._id });
-      }
-    } else {
+    if (!errors.isEmpty()) {
       return response.status(400).send({
         message: 'invalid body request',
         details: errors.array(),
       });
+    }
+
+    const existedUser = await User.findOne({ email });
+
+    if (existedUser) {
+      return response.status(409).send({
+        message: 'user with this email is already registered',
+      });
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const newUser = await User.create({
+        ...request.body,
+        password: hashedPassword,
+      });
+
+      const tokens = tokenService.generateTokens({ _id: newUser._id });
+      await tokenService.save(newUser._id, tokens.refreshToken);
+
+      return response.status(201).send({ ...tokens, userId: newUser._id });
     }
   } catch (error) {
     if (error instanceof Error) {
@@ -47,6 +47,46 @@ export const registerUser = async (
     } else {
       return response.status(500).json({
         message: `failed to singUp, error text: ${error}`,
+      });
+    }
+  }
+};
+
+export const loginUser = async (request: Request, response: Response): Promise<Response> => {
+  const { email, password } = request.body;
+
+  try {
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      return response.status(401).send({
+        message: 'email not found',
+      });
+    }
+
+    const isPasswordEqual = await bcrypt.compare(password, existingUser.password);
+
+    if (!isPasswordEqual) {
+      return response.status(401).send({
+        message: 'invalid password',
+      });
+    }
+
+    const tokens = tokenService.generateTokens({ _id: existingUser._id });
+    await tokenService.save(existingUser._id, tokens.refreshToken);
+
+    return response.status(200).send({
+      ...tokens,
+      userId: existingUser._id,
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      return response.status(500).json({
+        message: `failed to singIn, error text: ${error.message}`,
+      });
+    } else {
+      return response.status(500).json({
+        message: `failed to singIn, error text: ${error}`,
       });
     }
   }
--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import { registerUser } from '@/controllers/auth.controller';
+import { loginUser, registerUser } from '@/controllers/auth.controller';
 import { registerValidator } from '@/utils/validators/auth.validators';
 
 const router = express.Router({ mergeParams: true });
 
 router.post('/signUp', registerValidator, registerUser);
+router.post('/signIn', loginUser);
 
 export default router;
